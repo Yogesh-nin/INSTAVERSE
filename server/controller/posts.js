d89d3a4f5530bcaf737967cc961a8b60c72ab9ff
Yogesh <yogesh.204081@sggscc.ac.in>
2022-01-04 17:45:58 +0530

    creating models and schema

---
+++ server/controller/posts.js
@@ -0,0 +1,27 @@
+import PostMessage from "../models/postMessage";
+
+export const getPosts = async (req, res) =>{
+    try{
+        const postMessages = await PostMessage.find()
+        console.log('Postmessages')
+
+        res.status(200).json(postMessages)
+    }catch(error)
+    {
+        res.status(404).json({message: error.message})
+    }
+    
+}
+
+export const creatPosts = async (req, res) => {
+    const body = req.body;
+    const newPost = new PostMessage(body)
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+    res.send('Post creation');
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import {getPosts} from '../controller/posts.js'
 
 const router = express.Router();
 
-router.get('./', (req, res)=>{
-    res.send('Posts works!')
-})
+router.get('./', getPosts);
 
 export default router
