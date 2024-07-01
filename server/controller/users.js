d1027f847bf16ee0262ecc040428a3679de2c8a6
Yogesh Chandra <76822009+Yogesh-nin@users.noreply.github.com>
2024-07-01 21:11:21 +0530

    status fixed

--- server/controller/users.js
+++ server/controller/users.js
@@ -13,7 +13,7 @@ export const signin = async (req, res) =>{
 
         if(!isPasswordCorrect) return res.status(404).json({message: "Invalid Credentials"})
         const token = jwt.sign({email: existingUser.email, id: existingUser._id}, 'qwertyasdf', {expiresIn: '1h'})
-        res.status(400).json({result: existingUser, token})
+        res.status(200).json({result: existingUser, token})
     } catch (error) {
         res.status(500).json({message: "something went wrong"})
     }
@@ -31,7 +31,7 @@ export const signup = async (req, res) =>{
         const hashedPassword = await bcrypt.hash(password, 12)
         const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`})
         const token = jwt.sign({email: result.email, id: result._id}, 'qwertyasdf', {expiresIn: '1h'})
-        res.status(400).json({result, token})
+        res.status(200).json({result, token})
 
 
     } catch (error){
