bf3904f601bc805229bf6f9ddf48693e664628e7
Yogesh Chandra <76822009+Yogesh-nin@users.noreply.github.com>
2024-07-01 22:17:30 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,10 +4,10 @@ import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('./', getPosts);
-router.post('./', auth, createPost);
-router.patch('./:id', auth, updatePost);
-router.patch('./:id/likePost', auth, likePost);
-router.delete('./:id', auth, deletePost);
+router.get('/', getPosts);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.patch('/:id/likePost', auth, likePost);
+router.delete('/:id', auth, deletePost);
 
 export default router
